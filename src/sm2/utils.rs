f92551deb4df576233bfacb0c45b8dbf3f669157
hammeWang <dsw0602@foxmail.com>
2020-04-16 11:43:28 +0800

    add point_to_compress

--- src/sm2/utils.rs
+++ src/sm2/utils.rs
@@ -1,5 +1,6 @@
 use sm2::field::FieldElem;
 use arrayref::{array_mut_ref, array_ref};
+use sm2::ecc::{Point, EccCtx};
 
 // from [u8; 65] to [u8; 32]
 pub fn full_pk_to_compress(full: &[u8]) -> Result<[u8; 33], ()> {
@@ -18,4 +19,11 @@ pub fn full_pk_to_compress(full: &[u8]) -> Result<[u8; 33], ()> {
 		*array_mut_ref!(ret, 1, 32) = *array_ref!(full, 1, 32);
 		Ok(ret)
 	}
+}
+
+pub fn point_to_compress(p: &Point) -> [u8; 33] {
+	let ecc = EccCtx::new();
+	// get the compressed public key
+	let pk_bytes = ecc.point_to_bytes(p, true);
+	*array_ref!(pk_bytes, 0, 33)
 }
